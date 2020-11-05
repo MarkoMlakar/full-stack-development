f17ed24b8b3d46ab65008a855b05661e117fe4ed
Marko Mlakar <markomlakar2@gmail.com>
2020-11-05 17:19:58 +0100

    Implement Promises
    * Add Promises to CRUD operations

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,3 +1,5 @@
+/* Mock Database */
+
 let userArray = [
   { id: 1, name: "Marko", lastname: "Mlakar" },
   { id: 2, name: "Nika", lastname: "Maltar" },
@@ -5,27 +7,54 @@ let userArray = [
 ];
 
 module.exports = {
-  findAll: () => userArray,
-  findById: (id) => {
-    let user = userArray.find((user) => user.id == id);
-    return user;
-  },
-  addUser: (body) => {
-    let userId = userArray.length + 1;
-    body["id"] = userId;
-    userArray.push(body);
-    return userArray;
-  },
-  updateUser: (body) => {
-    let userId = userArray.findIndex((user) => user.id == body.id);
-    userArray[userId] = body;
-    return userArray;
-  },
-  deleteUser: (id) => {
-    let res = userArray.filter((user) => {
-      return user.id != id;
-    });
-    userArray = res;
-    return res;
-  },
+  findAll: () =>
+    new Promise((resolve) => {
+      resolve(userArray);
+    }),
+
+  findById: (id) =>
+    new Promise((resolve, reject) => {
+      let user = userArray.find((user) => user.id == id);
+      if (user == undefined) {
+        reject("User does not exist");
+      } else {
+        resolve(user);
+      }
+    }),
+  addUser: (body) =>
+    new Promise((resolve, reject) => {
+      if (!body.hasOwnProperty("name")) {
+        reject("User object must have a property name");
+      } else {
+        let userId = userArray.length + 1;
+        body["id"] = userId;
+        userArray.push(body);
+        resolve(userArray);
+      }
+    }),
+  updateUser: (body) =>
+    new Promise((resolve, reject) => {
+      const userId = userArray.findIndex((user) => user.id == body.id);
+
+      if (userId == -1) {
+        reject("Can not update because the user does not exist");
+      } else if (!body.hasOwnProperty("name")) {
+        reject("User object must have a property name");
+      } else {
+        userArray[userId] = body;
+        resolve(userArray);
+      }
+    }),
+  deleteUser: (id) =>
+    new Promise((resolve, reject) => {
+      let userId = userArray.find((user) => user.id == id);
+      if (userId == undefined) {
+        reject("Can not delete because the user does not exist");
+      } else {
+        userArray = userArray.filter((user) => {
+          return user.id != id;
+        });
+        resolve(userArray);
+      }
+    }),
 };
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -7,27 +7,57 @@ const db = require("../models/user");
 
 /* GET - Get all Users. */
 router.get("/", function(req, res, next) {
-  res.json(db.findAll());
+  db.findAll()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 /* GET - Get User by ID. */
 router.get("/:id", function(req, res, next) {
-  res.json(db.findById(req.params.id));
+  db.findById(req.params.id)
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-/* POST - Create new User */
-router.post("/create", function(req, res, next) {
-  res.json(db.addUser(req.body));
+/* POST - Add new User */
+router.post("/add", function(req, res, next) {
+  db.addUser(req.body)
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 /* PUT - Update User */
 router.put("/update", function(req, res, next) {
-  res.json(db.updateUser(req.body));
+  db.updateUser(req.body)
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-/* DELETE Delete User */
+/* DELETE - Delete User */
 router.delete("/delete/:id", function(req, res, next) {
-  res.json(db.deleteUser(req.params.id));
+  db.deleteUser(req.params.id)
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
